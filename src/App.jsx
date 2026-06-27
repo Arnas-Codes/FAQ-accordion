20c1453b1cfc3a2b97f4d72a012e50ef7218a0af
Arnas Foysal <arnas.codes@gmail.com>
2026-06-27 17:04:41 +0600

    Update App.jsx
    a

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,6 @@ const faqData = [
     answer:
       "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
   },
-  {
-    id: 4,
-    question: "How can I get help if I'm stuck on a challenge?",
-    answer:
-      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
-  },
 ];
 
 const App = () => {
